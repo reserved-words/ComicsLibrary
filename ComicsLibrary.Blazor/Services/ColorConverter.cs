ca3f54e8d5a847a1e436c075f985ed2a96e0e03b
Rhian <rhian.gwawr@gmail.com>
2021-04-05 14:48:01 +0100

    Sorted progress bar colours

---
+++ ComicsLibrary.Blazor/Services/ColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using ComicsLibrary.Blazor.Model;
+using ComicsLibrary.Common;
+using MudBlazor;
+
+namespace ComicsLibrary.Blazor
+{
+    public static class ColorConverter
+    {
+        public static Color Convert(string color)
+        {
+            return color switch
+            {
+                "purple" => Color.Primary,
+                "pink" => Color.Secondary,
+                "turquoise" => Color.Tertiary,
+                "blue" => Color.Info,
+                "green" => Color.Success,
+                "gold" => Color.Warning,
+                "red" => Color.Error,
+                _ => Color.Dark,
+            };
+        }
+
+        public static Color MudColor(this Series series) => Convert(series.Color);
+        public static Color MudColor(this NextComicInSeries book) => Convert(book.Color);
+    }
+}
