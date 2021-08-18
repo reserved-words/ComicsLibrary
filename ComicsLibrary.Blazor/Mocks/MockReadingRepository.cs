53829ac85808a54eeb5f3548bf21f5c4400d8868
Rhian Adams <rhian.gwawr@gmail.com>
2021-08-18 23:07:26 +0100

    Further development of mock repositories

--- ComicsLibrary.Blazor/Mocks/MockReadingRepository.cs
+++ ComicsLibrary.Blazor/Mocks/MockReadingRepository.cs
@@ -1,7 +1,9 @@
 ﻿using ComicsLibrary.Blazor.Services;
 using ComicsLibrary.Common;
+using ComicsLibrary.Common.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ComicsLibrary.Blazor.Mocks
@@ -10,54 +12,32 @@ namespace ComicsLibrary.Blazor.Mocks
     {
         public async Task<List<NextComicInSeries>> GetNextToRead(bool refreshCache)
         {
-            return new List<NextComicInSeries>
+            var list = new List<NextComicInSeries>();
+
+            var allSeries = MockSeriesRepository.GetShelf(Shelf.Reading);
+                
+            foreach (var series in allSeries)
             {
-                new NextComicInSeries
+                var books = MockSeriesRepository.AllBooks[series.Id];
+                var firstUnread = books.First(b => !b.IsRead);
+                list.Add(new NextComicInSeries
                 {
-                    Id = 105,
-                    SeriesId = 1,
-                    SeriesTitle = "Black Widow",
-                    Years = "2020 - Present",
-                    Publisher = "M",
-                    Color = "red",
-                    IssueTitle = "#5",
-                    ImageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/9/70/6026d186cfbc7.jpg",
-                    ReadUrl = "https://read.marvel.com/#/book/56005",
-                    UnreadBooks = 2,
-                    Creators = "Kelly Thompson",
-                    Progress = 45
-                },
-                new NextComicInSeries
-                {
-                    Id = 201,
-                    SeriesId = 2,
-                    SeriesTitle = "Captain Marvel",
-                    Years = "2002 - 2004",
-                    Publisher = "M",
-                    Color = "red",
-                    IssueTitle = "#1",
-                    ImageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/f/a0/5b17d999abbdb.jpg",
-                    ReadUrl = "https://read.marvel.com/#/book/10389",
-                    UnreadBooks = 25,
-                    Creators = "Kelly Thompson",
-                    Progress = 0
-                },
-                new NextComicInSeries
-                {
-                    Id = 301,
-                    SeriesId = 3,
-                    SeriesTitle = "Harley Quinn",
-                    Years = "2016 - Present",
-                    Publisher = "DC",
-                    Color = "blue",
-                    IssueTitle = "Vol. 1",
-                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439438/439438._SX312_QL80_TTD_.jpg",
-                    ReadUrl = "https://www.comixology.co.uk/comic-reader/74153/439438",
-                    UnreadBooks = 10,
-                    Creators = "Amanda Conner",
-                    Progress = 0
-                }
-            };
+                    Id = firstUnread.Id,
+                    SeriesId = series.Id,
+                    SeriesTitle = series.Title,
+                    Years = series.Years,
+                    Publisher = series.Publisher,
+                    Color = series.Color,
+                    IssueTitle = firstUnread.IssueTitle,
+                    ImageUrl = firstUnread.ImageUrl,
+                    ReadUrl = firstUnread.ReadUrl,
+                    UnreadBooks = books.Count(b => !b.IsRead),
+                    Creators = "",
+                    Progress = series.Progress
+                });
+            }
+
+            return list;
         }
     }
 }
--- ComicsLibrary.Blazor/Mocks/MockSeriesRepository.cs
+++ ComicsLibrary.Blazor/Mocks/MockSeriesRepository.cs
@@ -1,5 +1,4 @@
-﻿using ComicsLibrary.Blazor.Services;
-using ComicsLibrary.Common;
+﻿using ComicsLibrary.Common;
 using ComicsLibrary.Common.Data;
 using System;
 using System.Collections.Generic;
@@ -8,73 +7,29 @@ using System.Threading.Tasks;
 
 namespace ComicsLibrary.Blazor.Mocks
 {
-    public class MockSeriesRepository : Blazor.Services.ISeriesRepository
+    public class MockSeriesRepository : Services.ISeriesRepository
     {
-        private static List<LibrarySeries> AllSeries = new List<LibrarySeries>
-        {
-            new LibrarySeries
-            {
-                Id = 1,
-                Title = "Black Widow (2020 - Present)",
-                ImageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/c/40/5f3d36dc73d2a.jpg",
-                Progress = 45,
-                Publisher = "Marvel",
-                PublisherIcon = "M",
-                Color = "red",
-                Shelf = Shelf.Reading
-            },
-            new LibrarySeries
-            {
-                Id = 3,
-                Title = "Harley Quinn (2016-)",
-                ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439438/439438._SX312_QL80_TTD_.jpg",
-                Progress = 0,
-                Publisher = "DC Comics",
-                PublisherIcon = "DC",
-                Color = "blue",
-                Shelf = Shelf.ToReadNext
-            },
-            new LibrarySeries
-            {
-                Id = 4,
-                Title = "Alias (2001 - 2003)",
-                ImageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/4/20/56966d674b06d.jpg",
-                Progress = 100,
-                Publisher = "Marvel",
-                PublisherIcon = "M",
-                Color = "red",
-                Shelf = Shelf.Finished
-            },
-            new LibrarySeries
-            {
-                Id = 5,
-                Title = "Action Comics (2016-)",
-                ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439423/439423._SX312_QL80_TTD_.jpg",
-                Progress = 20,
-                Publisher = "DC Comics",
-                PublisherIcon = "DC",
-                Color = "blue",
-                Shelf = Shelf.Archived
-            },
-            new LibrarySeries
-            {
-                Id = 6,
-                Title = "Bitch Planet",
-                ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/294324/294324._SX360_QL80_TTD_.jpg",
-                Progress = 15,
-                Publisher = "Image Comics",
-                PublisherIcon = "I",
-                Color = "pink",
-                Shelf = Shelf.Archived
-            }
-        };
+        public static readonly List<LibrarySeries> AllSeries;
+        public static readonly Dictionary<int, List<Comic>> AllBooks;
 
-        public async Task<List<Model.Series>> GetShelf(Shelf shelf, bool refreshCache)
+        static MockSeriesRepository()
+        {
+            AllSeries = new List<LibrarySeries>();
+            AllBooks = new Dictionary<int, List<Comic>>();
+            PopulateSeries();
+        }
+
+        public static List<Model.Series> GetShelf(Shelf shelf)
         {
             return AllSeries
                 .Where(s => s.Shelf == shelf)
                 .Select(s => new Model.Series(s))
                 .ToList();
+        }
+
+        public async Task<List<Model.Series>> GetShelf(Shelf shelf, bool refreshCache)
+        {
+            return GetShelf(shelf);
         }
 
         public async Task<bool> UpdateShelf(Model.Series series, Shelf newShelf)
@@ -91,6 +46,99 @@ namespace ComicsLibrary.Blazor.Mocks
             }
 
             return true;
+        }
+
+        private static void PopulateSeries()
+        {
+            AllSeries.AddSeries(Shelf.Reading, "M", "Black Widow", 2020, null, "https://i.annihil.us/u/prod/marvel/i/mg/c/40/5f3d36dc73d2a.jpg")
+                .AddBook("#1", true, "")
+                .AddBook("#2", true, "")
+                .AddBook("#3", true, "")
+                .AddBook("#4", true, "")
+                .AddBook("#5", false, "https://i.annihil.us/u/prod/marvel/i/mg/9/70/6026d186cfbc7.jpg", "https://read.marvel.com/#/book/56005");
+
+            AllSeries.AddSeries(Shelf.ToReadNext, "DC", "Harley Quinn", 2016, null, "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439438/439438._SX312_QL80_TTD_.jpg")
+                .AddBook("Vol. 1", false, "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439438/439438._SX312_QL80_TTD_.jpg", "https://www.comixology.co.uk/comic-reader/74153/439438")
+                .AddBook("Vol. 2", false, "")
+                .AddBook("Vol. 3", false, "")
+                .AddBook("Vol. 4", false, "")
+                .AddBook("Vol. 5", false, "");
+            
+            AllSeries.AddSeries(Shelf.Finished, "M", "Alias", 2001, 2003, "https://i.annihil.us/u/prod/marvel/i/mg/4/20/56966d674b06d.jpg");
+            
+            AllSeries.AddSeries(Shelf.Archived, "DC", "Action Comics", 2016, null, "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439423/439423._SX312_QL80_TTD_.jpg");
+            
+            AllSeries.AddSeries(Shelf.Archived, "I", "Bitch Planet", null, null, "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/294324/294324._SX360_QL80_TTD_.jpg");
+        }
+    }
+
+    public static class HelperMethods
+    {
+        public static LibrarySeries AddSeries(this List<LibrarySeries> list, Shelf shelf, string publisher, string title, int? startYear, int? endYear, string imageUrl)
+        {
+            var id = list.Count + 1;
+
+            var years = startYear == null
+                ? ""
+                : endYear == null
+                ? $" ({startYear} - Present)" 
+                : startYear == endYear
+                ? $" ({startYear})"
+                : $" ({startYear} - {endYear})";
+
+            var fullTitle = $"{title}{years}";
+
+            var (publisherName, colour) = publisher switch
+            {
+                "M" => ("Marvel Comics", "red"),
+                "DC" => ("DC Comics", "blue"),
+                "I" => ("Image Comics", "pink"),
+                "B" => ("BOOM! Studios", "yellow"),
+                _ => ("Unknown", "black")
+            };
+
+            var series = new LibrarySeries
+            {
+                Id = id,
+                Title = fullTitle,
+                ImageUrl = imageUrl,
+                Progress = 0, // calculate when books are added
+                Publisher = publisherName,
+                PublisherIcon = publisher,
+                Color = colour,
+                Shelf = shelf
+            };
+
+            list.Add(series);
+
+            MockSeriesRepository.AllBooks.Add(id, new List<Comic>());
+
+            return series;
+        }
+
+        public static LibrarySeries AddBook(this LibrarySeries series, string title, bool read, string imageUrl, string readUrl = null)
+        {
+            var numberOfComicsAdded = MockSeriesRepository.AllBooks.Sum(s => s.Value.Count);
+
+            var comic = new Comic
+            {
+                Id = numberOfComicsAdded + 1,
+                IssueTitle = title,
+                SeriesTitle = series.Title,
+                IsRead = read,
+                ImageUrl = imageUrl, 
+                ReadUrl = readUrl
+            };
+
+            var seriesComics = MockSeriesRepository.AllBooks[series.Id];
+
+            seriesComics.Add(comic);
+
+            var progress = 100 * (double)seriesComics.Count(c => c.IsRead) / (double)seriesComics.Count;
+
+            series.Progress = (int)Math.Round(progress, 0);
+
+            return series;
         }
     }
 }
